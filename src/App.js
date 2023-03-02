3c36527bb607d9b331801c332919c40318fa1e36
ocmino <ocmino@gmail.com>
2023-03-02 09:05:24 +0100

    sdfsfsdf

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const models = [
+    {
+      name: "Astronaut",
+      src: "https://modelviewer.dev/shared-assets/models/Astronaut.glb",
+    },
+    {
+      name: "Neil",
+      src: "https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb",
+    },
+    {
+      name: "Car",
+      src: "https://modelviewer.dev/shared-assets/models/glTF-Sample-Models/2.0/ToyCar/glTF-Binary/ToyCar.glb",
+    },
+  ];
+
+  const [modelViewerStyle, setModelViewerStyle] = useState({});
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth >= 768) {
+        setModelViewerStyle({
+          width: "80vw",
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        });
+      } else {
+        setModelViewerStyle({
+          width: "100vw",
+          height: "calc(100vh - 50px)",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        });
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const changeModel = (e) => {
+    const modelViewer = document.querySelector("model-viewer");
+    modelViewer.src = e.target.value;
+  };
+
+  return (
+    <div className="App">
+      <div style={modelViewerStyle}>
+        <model-viewer
+          style={{ width: "100%", height: "100%" }}
+          src={models[0].src}
+          auto-rotate
+          camera-controls
+        ></model-viewer>
+      </div>
+
+      <div className="configurator">
+        {models.map((model) => (
+          <button
+            className="button"
+            key={model.name}
+            value={model.src}
+            onClick={(e) => changeModel(e)}
+          >
+            {model.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
